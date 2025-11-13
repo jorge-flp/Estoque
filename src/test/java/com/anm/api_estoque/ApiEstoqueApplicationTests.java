8ce6563d343ff3911fbccd91bbe59e98bf752b37
jorge-flp <jorgefeliperodrigues841@gmail.com>
2025-11-13 19:33:09 -0300

    Projeto criado.

---
+++ src/test/java/com/anm/api_estoque/ApiEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anm.api_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
